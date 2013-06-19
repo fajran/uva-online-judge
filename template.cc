7e8bd7780f43403d0524af98cbce5850292fe88b
Fajran Iman Rusadi <fajran@gmail.com>
2013-06-19 20:29:50 +0200

    Initial commit

---
+++ template.cc
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <set>
+#include <map>
+#include <sstream>
+
+#include <strings.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <string.h>
+
+using namespace std;
+
+int main() {
+
+}
+
+// vim: ts=2 sts=2
+
