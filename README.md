8f3d1b2ac69c1f8e59d0d46fbca71835291c21b2
Fajran Iman Rusadi <fajran@gmail.com>
2013-09-04 14:17:28 +0200

    Add test runner script and put to Travis-CI

--- README.md
+++ README.md
@@ -4,3 +4,6 @@ Not all source codes of the problems that I have solved are here though.
 * [uHunt profile](http://uhunt.felix-halim.net/id/91745)
 
 Please use the source codes for learning purpose only. Don't use them as your own solutions.
+
+https://travis-ci.org/fajran/uva-online-judge.png?branch=master
+
---
+++ test-all.sh
@@ -0,0 +1,69 @@
+#!/bin/bash
+
+Color_Off='\e[0m'       # Text Reset
+IYellow='\e[0;93m'      # Yellow
+BIBlue='\e[1;94m'       # Blue
+IRed='\e[0;91m'         # Red
+On_Black='\e[40m'       # Black
+IGreen='\e[0;92m'       # Green
+
+if [ -z $CXX ]; then
+  CXX=g++
+fi
+
+check() {
+  dir=$1
+  pushd $dir > /dev/null 2>&1
+
+  srcs=$(ls *.cc 2> /dev/null | grep -v template.cc)
+  if [ -z "$srcs" ]; then
+    popd > /dev/null 2>&1
+    return 1
+  fi
+
+  title=$(git log --pretty=oneline . | head -n1 | cut -d' ' -f 2-)
+  echo -e ${BIBlue}$title${Color_Off}
+  echo
+
+  if [ ! -f input.txt -o ! -f output.txt ]; then
+    echo -e "${IYellow}W: No input or output files${Color_Off}"
+    echo
+    popd > /dev/null 2>&1
+    return 2
+  fi
+
+  ret=0
+  for src in $srcs
+  do
+    echo "> $src"
+    if $CXX -o bin $src -lm -lcrypt -O2 -pipe -DONLINE_JUDGE > /dev/null 2>&1
+    then
+      /usr/bin/time -f "%E real, %U user, %S sys" -o .time.txt ./bin < input.txt > .cmp-output.txt
+      cat .time.txt | sed 's/^/  /'
+      diff output.txt .cmp-output.txt > /dev/null 2>&1
+      if [ $? -ne 0 ]; then
+        echo -e "  ${IRed}E: Invalid result${Color_Off}"
+        ret=3
+      else
+        echo -e "  ${IGreen}OK${Color_Off}"
+      fi
+    else
+      echo -e "  ${IRed}E: Compile error${Color_Off}"
+      ret=3
+    fi
+    echo
+  done
+
+  popd > /dev/null 2>&1
+
+  return $ret
+}
+
+find . -type d -not -iwholename '*.git*' | while read dir
+do
+  check $dir
+  if [ $? -eq 3 ]; then
+    exit 3
+  fi
+done
+
